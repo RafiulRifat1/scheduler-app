896a48552d00065d9cc22126769c41c4a591a48e
MD Rafiul Islam Rifat <rafiul2071@gmail.com>
2025-08-14 22:15:04 +0600

    signIn page image div style change

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -5,17 +5,15 @@ import { SignupForm } from "./signupform"
 
 const Signup = () => {
   return (
-    <div className='flex justify-center w-full h-screen pt-4'>
+    <div className='flex justify-center gap-20 w-full h-screen pt-4'>
 
-      <div className='relative w-1/2 h-full hidden md:block '>
-          <div className='absolute w-[700px] h-[777px] '>
+      <div className='relative w-1/2 h-full hidden md:block max-w-[700px] max-h-[777px] '>
             <Image 
             src='/signup.webp' 
             alt='signup img'   
             fill={true} 
-            className='object-cover' />
-            <div className="absolute inset-0 bg-[linear-gradient(311deg,rgba(244,17,50,0)_0%,rgba(244,17,50,0.5)_100%)]"/>          
-        </div>
+            className='object-cover rounded-2xl'/>
+            <div className="absolute rounded-2xl inset-0 bg-[linear-gradient(311deg,rgba(244,17,50,0)_0%,rgba(244,17,50,0.5)_100%)]"/>          
        </div>
        <SignupForm/>
     </div>
